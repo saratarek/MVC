ed8c45ccd258ec88bb0f514aae18985268d0a3cc
Sara Tarek <saraagamy972@gmail.com>
2020-05-04 17:17:42 +0200

    Lab 1

---
+++ lab1mvc/lab1mvc/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class Employee {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public int Phone { get; set; }
+
+        }
+
+         [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RegisterForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RegisterForm(Employee employee)
+        {
+            if (employee != null & employee.Email != null && employee.Name != null)
+            {
+                ViewBag.Name = employee.Name;
+                return View("Welcome");
+            }
+            return View();
+        }
+
+    }
+}
